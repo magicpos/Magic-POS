bd9f81139ac12f0940d9eb3690d0cc5ea3bfcd21
Abhishek Srivastava <abhisheksri.sri.sri@gmail.com>
2018-02-18 10:33:52 +0530

    Blank Mobile Application project added

---
+++ MobileApplication/app/src/main/java/com/example/buddy_000/magicpos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.buddy_000.magicpos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
